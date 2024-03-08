716b3e437ab0820dd328888064c2d107d22f1425
Eduardo Almeida <almeedusa@gmail.com>
2024-03-08 11:41:09 -0300

    Deleting connection.py

--- connection.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-connection = mysql.connector.connect(
-    #change names after creat a database
-    host='localhost',
-    user='client',
-    password='client',
-    database='school'
-)
